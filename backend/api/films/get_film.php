15eb94c6c0ce819bca2e871e62068ebe41b6d123
Roberto <robertspacecraft@gmail.com>
2026-02-03 08:30:17 +0100

    nuevos ajustes en Dockerfile y .conf

--- backend/api/films/get_film.php
+++ backend/api/films/get_film.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . "/../../utils/utils.php";
+require_once __DIR__ . "/../../utils/utils.php"; 
 require_once __DIR__ . "/../db/db.php";
 
 // Cabecera para asegurar que la respuesta sea JSON
