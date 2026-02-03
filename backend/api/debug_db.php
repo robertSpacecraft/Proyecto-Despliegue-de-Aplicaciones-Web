924c37e48e6ac7fe90b2394bcfa3ede353aadec4
Roberto <robertspacecraft@gmail.com>
2026-02-03 09:29:30 +0100

    cambio de ruta en el debug

--- backend/api/debug_db.php
+++ backend/api/debug_db.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require_once __DIR__ . "/../db/db.php";
+require_once __DIR__ . "/db/db.php";
 
 echo "Intentando conectar a: " . getenv("DB_HOST") . "<br>";
 
